5a59bd5eae8c1b01e606e4cdaf18d7f6db3b4f3f
0xabstracted <vskbr23@gmail.com>
2022-11-13 22:41:20 +0530

    changed idl without identifier

---
+++ programs/apl-payment-manager/src/lib.rs
@@ -0,0 +1,42 @@
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+use {anchor_lang::prelude::*, instructions::*};
+
+declare_id!("APMT8qWbAUapzsBoxx1ACrt37JFWS76Bkn9PUre5ybyn");
+
+#[program]
+pub mod apl_payment_manager {
+    use super::*;
+
+    pub fn init(ctx: Context<InitCtx>, ix: InitIx) -> Result<()> {
+        init::handler(ctx, ix)
+    }
+
+    pub fn manage_payment(ctx: Context<HandlePaymentCtx>, payment_amount: u64) -> Result<()> {
+        handle_payment::handler(ctx, payment_amount)
+    }
+
+    pub fn handle_payment_with_royalties<'key, 'accounts, 'remaining, 'info>(
+        ctx: Context<'key, 'accounts, 'remaining, 'info, HandlePaymentWithRoyaltiesCtx<'info>>,
+        payment_amount: u64,
+    ) -> Result<()> {
+        handle_payment_with_royalties::handler(ctx, payment_amount)
+    }
+
+    pub fn handle_native_payment_with_royalties<'key, 'accounts, 'remaining, 'info>(
+        ctx: Context<'key, 'accounts, 'remaining, 'info, HandleNativePaymentWithRoyaltiesCtx<'info>>,
+        payment_amount: u64,
+    ) -> Result<()> {
+        handle_native_payment_with_royalties::handler(ctx, payment_amount)
+    }
+
+    pub fn close(ctx: Context<CloseCtx>) -> Result<()> {
+        close::handler(ctx)
+    }
+
+    pub fn update(ctx: Context<UpdateCtx>, ix: UpdateIx) -> Result<()> {
+        update::handler(ctx, ix)
+    }
+}
